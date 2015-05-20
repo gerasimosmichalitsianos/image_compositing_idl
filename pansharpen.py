f28dbcedd739428e3138043f68c336eae5484d56
Gerasimos Michalitsianos <gerasimosmichalitsianos@gmail.com>
2015-05-20 18:55:02 -0400

    Update pansharpen.py

--- pansharpen.py
+++ pansharpen.py
@@ -2,7 +2,8 @@
 #!/usr/bin/env python
 import os
 import sys
-import gc 
+import gc
+import math 
 import numpy as np
 import warnings as warn 
 from osgeo import osr, gdal
@@ -15,7 +16,6 @@ class SatelliteImage(object):
     imagery. This class makes use coarser red, green, blue, and NIR, bands, as well as 
     the finer panchromatic band. Methods include Brovey and PCA pan-sharpening 
     methods, NDVI, and other attributes. More to come. 
-
     @author: 
     Gerasimos Andreas Michalitsianos 
     gerasimosmichalitsianos@gmail.com 
@@ -64,25 +64,23 @@ class SatelliteImage(object):
         driv = gdal.GetDriverByName('GTiff')
         if not outname.endswith('.tif'): 
             outname=outname+'.tif'
-        elif not ('str' not in str(type(outname))): 
+        elif type(outname) is not str: 
             raise TypeError('saveimg(img,outname): second argument should be a string (.tif)')
-        elif not ('numpy.ndarray' not in str(type(img))): 
+        elif type(img) is not np.ndarray: 
             raise TypeError('saveimg(img,outname): first argument should be a Numpy array')
         
-        # ---- compute the numpy of data layers 
-        if len(img.shape) == 3: 
+        # ---- compute the numpy of data layers
+        if len(img.shape)==3: 
             nbands = img.shape[0]
         else: 
             nbands = 1
 
-        if img.shape == self.pan.shape:
-
+        if img.shape[1:] == self.pan.shape: 
             dst = driv.Create(outname, self.dspan.RasterXSize, self.dspan.RasterYSize, nbands, gdal.GDT_Float32)
             dst.SetGeoTransform(self.dspan.GetGeoTransform())
             dst.SetProjection(self.dspan.GetProjection())
 
-        elif img.shape == self.red.shape:
-
+        elif img.shape[1:] == self.red.shape:
             dst = driv.Create(outname, self.dsmulti.RasterXSize, self.dsmulti.RasterYSize, nbands, gdal.GDT_Float32)
             dst.SetGeoTransform(self.dsmulti.GetGeoTransform())
             dst.SetProjection(self.dsmulti.GetProjection())
@@ -90,20 +88,92 @@ class SatelliteImage(object):
         if nbands == 1: 
             dst.GetRasterBand(1).WriteArray(img)
         else: 
-            for band in range(0,img.shape[0]): dst.GetRasterBand(band+1).WriteArray(img[band,:,:])
-
+            for band in range(img.shape[0]): dst.GetRasterBand(band+1).WriteArray(img[band,:,:])
         dst=None
         del dst
 
-    def pcasharpen(self,band):
+    def ihsSharpen(self):
+
+        '''
+        This method returns pan-sharpened NDVI resulting from 
+        the IHS pan-sharpening of red and NIR bands. 
+        
+        Source paper: 
+        Application of IHS-Sharpening Techniques to Ikonos Images 
+        http://ieee.uniparthenope.it/chapter/_private/proc10/24.pdf
+        
+        '''
+
+        # ---- first we have to resize RGB,NIR bands to same dimensions as panchromatic image using nearest neighbor interpolation
+        nrows,ncols = self.pan.shape
+        panfloat     = np.array(self.pan, dtype = np.float32)
+        redresized   = np.array(imresize(self.red, self.pan.shape, 'nearest'), dtype = np.float32)
+        blueresized  = np.array(imresize(self.blue, self.pan.shape, 'nearest'), dtype = np.float32)
+        greenresized = np.array(imresize(self.green, self.pan.shape, 'nearest'), dtype = np.float32)
+
+        # ---- [I,v1,v2] = [ 1/3, 1/3, 1/3 ; -sqrt(2)/6, -sqrt(2)/6, 2*sqrt(2)/6; 1/sqrt(2), 1/sqrt(2), 0] * [R; G; B]
+        
+        a = float(1)/3
+        b = -1*math.sqrt(2.0)/float(6)
+        c = 2.0*math.sqrt(2.0)/float(6)
+        d = float(1)/math.sqrt(2)
+        e = math.sqrt(2.0)
+
+        I  = (a*redresized) + (a*greenresized)  + (c*blueresized)
+        v1 = (b*redresized) + (b*greenresized)  + (c*blueresized)
+        v2 = (d*redresized) + (-d*greenresized) + (0.0*blueresized)
+        
+        # ---- compute fused R,G,B
+        redfused   = (1.0*panfloat) - (d*v1) + (d*v2)
+        greenfused = (1.0*panfloat) - (d*v1) - (d*v2)
+        bluefused  = (1.0*panfloat) + (e*v1) + (0.0*v2)
+
+        # ---- return pan-sharpened RGB in 3-layer numpy array
+        outsharpened = np.zeros((3,self.pan.shape[0],self.pan.shape[1]),dtype=np.float32)
+        outsharpened[0,:,:] = bluefused
+        outsharpened[1,:,:] = greenfused
+        outsharpened[2,:,:] = redfused
+        return outsharpened
+
+    def fihsSharpen(self):
+
+        '''
+        This method performs a generalized fast IHS transformation to 
+        pansharpen the RGB, NIR bands using the panchromatic band. 
+        
+        Source paper: 
+        An IHS-Based Fusion for Color Distortion Reduction and Vegetation Enhancement in IKONOS Imagery 
+        http://pgembeddedsystems.com/download/matlab/An%20IHS-Based%20Fusion%20for%20Color%20Distortion%20Reduction.pdf
+        
+        '''
+
+        panfloat     = np.array(self.pan, dtype = np.float32)
+        redresized   = np.array(imresize(self.red, self.pan.shape, 'nearest'), dtype = np.float32)
+        nirresized   = np.array(imresize(self.nir, self.pan.shape, 'nearest'), dtype = np.float32)
+        blueresized  = np.array(imresize(self.blue, self.pan.shape, 'nearest'), dtype = np.float32)
+        greenresized = np.array(imresize(self.green, self.pan.shape, 'nearest'), dtype = np.float32)
+
+        alpha = float(3)/4
+        I = (redresized+greenresized+blueresized+nirresized)/float(4)
+        redfused   = redresized   + (alpha*(panfloat - I))
+        greenfused = greenresized + (alpha*(panfloat - I))
+        bluefused  = blueresized  + (alpha*(panfloat - I))
+        nirfused   = nirresized   + (alpha*(panfloat - I))
+
+        # ---- return pan-sharpened RGB,NIR bands in 4-layer Numpy array
+        outsharpened = np.zeros((4,self.pan.shape[0],self.pan.shape[1]),dtype=np.float32)
+        
+        outsharpened[0,:,:] = bluefused
+        outsharpened[1,:,:] = greenfused
+        outsharpened[2,:,:] = redfused
+        outsharpened[3,:,:] = nirfused
+        return outsharpened
+        
+    def pcaSharpen(self):
         
         ''' 
-        This method performs principal component analysis (PCA) 
-        pan sharpening. The only argument should specify which 
-        band the user wants returned as a Numpy array, as in 
-        'red' for the pan-sharpened red Numpy array, 'blue' 
-        for the pan-sharpened blue array, 'nir' for the
-        pan-sharpened NIR array. 
+        This method returns a pan-sharpened NDVI using the pan-sharpening 
+        technique of principal component analysis. 
         ''' 
         
         if 'str' not in str(type(band)): 
@@ -167,55 +237,45 @@ class SatelliteImage(object):
 
         # The values obtained from the anti-transformation are not in the interval [0,1]:
         # so we have to interpolate them to bring them back in this range:
+        
         Rfin   = (Rfin-np.min(Rfin))/(np.max(Rfin)-np.min(Rfin))
         Gfin   = (Gfin-np.min(Gfin))/(np.max(Gfin)-np.min(Gfin))
         Bfin   = (Bfin-np.min(Bfin))/(np.max(Bfin)-np.min(Bfin))
         NIRfin = (NIRfin-np.min(NIRfin))/(np.max(NIRfin)-np.min(NIRfin))
 
-        if band == 'red':
-            return Rfin
-        if band == 'green':
-            return Gfin
-        if band == 'blue':
-            return Bfin
-        if band == 'nir':
-            return NIRfin
-        else:
-            return np.zeros(self.pan.shape) 
-        
+        # ---- return 3D numpy array holding pan-sharpened RGB,NIR bands (4 layers)
+        outsharpened = np.zeros((4,Rfin.shape[0],Rfin.shape[1]), dtype = np.float32)
+        outsharpened[0,:,:] = Bfin
+        outsharpened[1,:,:] = Gfin
+        outsharpened[2,:,:] = Rfin
+        outsharpened[3,:,:] = NIRfin
+        return outsharpened
+
 def main():
 
-    # ---- load RGB,NIR,pan images as arrays from Geotiff files 
-    dsblue  = gdal.Open('LC81850332013230LGN00_B2_clipped.TIF')
-    dsgreen = gdal.Open('LC81850332013230LGN00_B3_clipped.TIF')
-    dsred   = gdal.Open('LC81850332013230LGN00_B4_clipped.TIF')
-    dsnir   = gdal.Open('LC81850332013230LGN00_B5_clipped.TIF')
-    dspan   = gdal.Open('LC81850332013230LGN00_B8_clipped.TIF')
-
-    blue  = dsblue.GetRasterBand(1).ReadAsArray()
-    green = dsgreen.GetRasterBand(1).ReadAsArray()
-    red   = dsred.GetRasterBand(1).ReadAsArray()
-    nir   = dsnir.GetRasterBand(1).ReadAsArray()
-    pan   = dspan.GetRasterBand(1).ReadAsArray()
-
-    # ---- initiate a SatelliteImage object
-    mysatimg = SatelliteImage(dspan, dsred, red,green,blue,nir,pan)
-
-    # ---- start using SatelliteImage object to do stuff
+    panfname = '24DEC05QB020800005DEC24100239-P1BS-005682967010_01_P002_________AAE_0AAAAABAAAI0_TOA-Pan.tif'
+    msfname = '24DEC05QB020800005DEC24100239-M1BS-005682967010_01_P002_________GA_E0AAAAAAGAAC0_TOA-Multispec.tif'
     
-    ndvi = mysatimg.ndvi()  # compute NDVI and save it as a Geotiff 
-    mysatimg.saveimg(ndvi, 'ndvi.tif')
+    dsms = gdal.Open(msfname)
+    dspan = gdal.Open(panfname)
 
+    nir = dsms.GetRasterBand(4).ReadAsArray() 
+    red = dsms.GetRasterBand(3).ReadAsArray()
+    green = dsms.GetRasterBand(2).ReadAsArray()
+    blue = dsms.GetRasterBand(1).ReadAsArray()
+    pan = dspan.GetRasterBand(1).ReadAsArray()
+
+    satobj = SatelliteImage(dspan, dsms, red, green, blue, nir, pan)
     
-    redsharp = mysatimg.pcasharpen('red')
-    greensharp = mysatimg.pcasharpen('green')
-    bluesharp = mysatimg.pcasharpen('blue')
-    nirsharp = mysatimg.pcasharpen('nir')
+    del nir
+    del red
+    del green
+    del blue
+    del pan
+    gc.collect() 
 
-    mysatimg.saveimg(redsharp,'redsharp.tif')
-    mysatimg.saveimg(bluesharp,'bluesharp.tif')
-    mysatimg.saveimg(greensharp,'greensharp.tif')
-
+    fihsSharpened = satobj.fihsSharpen()
+    satobj.saveimg(fihsSharpened,'fihsSharpened.tif')
+    
 if __name__ == '__main__':
     main() 
-            
